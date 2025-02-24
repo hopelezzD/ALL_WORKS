1d4d09daa66834adeed63a52b985c40df235d609
hopelezzD <hopelezzD@gmail.com>
2025-02-24 18:21:25 +0200

    DZ8 NEW  верстка без адаптива финал

---
+++ DZ_8_NEW/JS/main.js
@@ -0,0 +1,16 @@
+ 
+function test(){
+
+
+    for (let index = 0; index < 10; index++) {
+        var b=0;
+        let a=0;
+        b+=index;
+        a+=index;
+        console.log(b);
+    }
+    console.log(b);
+}
+
+
+
